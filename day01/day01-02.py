fdf71d3790f6c48953d1a919fbcc45286f1faf4e
Hendrik Sauer <hendrik-sauer@mailbox.org>
2022-12-02 17:11:34 +0100

    Add day one

---
+++ day01/day01-02.py
@@ -0,0 +1,12 @@
+with open("puzzle_input") as puzzle_input:
+    lines = puzzle_input.readlines()
+    sums = []
+    sub_sum = 0
+    for l in lines:
+        if l == "\n":
+            sums.append(sub_sum)
+            sub_sum = 0
+        else:
+            sub_sum += int(l)
+    sums.sort()
+    print(sum(sums[-3:]))
